b65eae1a34ff78060ea2b606dd8565b03c6156c1
Prabhu0414 <167548318+Prabhu0414@users.noreply.github.com>
2025-08-16 11:37:16 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,26 +7,26 @@ export function About() {
   <h2 className="text-xl font-semibold mb-4  md:ml-3">About</h2>
 
  <div className="">
-  <a href="https://linkedin.com" className="block w-72 md:w-96 pb-4">
+  <a href="https://www.linkedin.com/in/tanmay-yadav-5b8271257?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" className="block w-72 md:w-96 pb-4">
   <div className="h-10 flex items-center px-4 gap-4 bg-gray-800 border-b-4 border-blue-500 text-white">
     <FaLinkedin size={20} />
     Linkedin Profile
   </div>
 </a>
 
-<a href="https://github.com"  className="block w-72 md:w-96 pb-4">
+<a href="https://github.com/Prabhu0414"  className="block w-72 md:w-96 pb-4">
   <div className="h-10 flex items-center px-4 gap-4 bg-gray-800 border-b-4 border-blue-500 text-white">
     <FaGithub size={20} />
     Github Profile
   </div>
 </a>
-<a href="https://leetcode.com" className="block w-72 md:w-96 pb-4">
+<a href="https://leetcode.com/u/tanmayyadav1410/" className="block w-72 md:w-96 pb-4">
   <div className="h-10 flex items-center gap-4 px-4 bg-gray-800 border-b-4 border-blue-500 text-white">
     <FaCode size={20} />
     Leetcode Profile
   </div>
 </a>
-<a href="https://twitter.com"  className="block w-72 md:w-96 pb-4">
+<a href="https://x.com/tanmay_yadav_?s=09"  className="block w-72 md:w-96 pb-4">
   <div className="h-10 flex items-center px-4 gap-4 bg-gray-800 border-b-4 border-blue-500 text-white">
     <FaTwitter size={20} />
     Twitter Handle
